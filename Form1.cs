7b23ae46e46ca287c5a501eb01e0b26897808f4f
Luke X Ramirez <lucasxavier@email.arizona.edu>
2021-09-11 21:29:26 -0700

    This shit works on my machine

--- Form1.cs
+++ Form1.cs
@@ -22,9 +22,19 @@ namespace Checkers
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            Pen myPen = new Pen(Color.Black);
+            /*
+            Pen myPen = new Pen(Color.Black, 5);
             Brush myBrush = new SolidBrush(Color.Blue);
             g.DrawLine(myPen, 200, 200, 400, 100);
+            */
+            //DialogResult result = MessageBox.Show("test", "caption");
+
+            string ImagesDirectory = Application.ExecutablePath;
+            //ImagesDirectory.Remove(ImagesDirectory.);
+
+            //Image newIamge = Image.FromFile(ImagesDirectory);
+
+            DialogResult result = MessageBox.Show(ImagesDirectory, "caption");
         }
     }
 }
