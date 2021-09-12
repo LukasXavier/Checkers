b140999514eb17557ca8beb5480e5d46fb5d6e5a
George Eric Mendoza <ericmendoza@email.arizona.edu>
2021-09-11 22:51:39 -0700

    9/11/21

---
+++ Piece.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers
+{
+    class Piece
+    {
+        private string color;
+        private int[] pos;
+
+        public Piece(string color)
+        {
+            this.color = color;
+            this.pos = new int[2];
+        }
+
+        public void setPos(int x, int y)
+        {
+            pos[0] = x;
+            pos[1] = y;
+        }
+    }
+}
